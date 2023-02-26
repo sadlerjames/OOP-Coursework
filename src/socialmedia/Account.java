e71318a811168fe1e538fb502fd31eefe46b084d
Joel Sawyer <joelsawyer2004@outlook.com>
2023-02-26 11:03:40 +0000

    Begin defining platform classes
    Co-authored-by: James Sadler <sadlerjames@users.noreply.github.com>

---
+++ src/socialmedia/Account.java
@@ -0,0 +1,40 @@
+package socialmedia;
+
+public class Account {
+    //Instance attributes
+
+    private int id;
+    private String handle;
+    private String description;
+    
+    public int createAccount(String handle) {
+        //toDo
+        return 4;
+    }
+
+    public int createAccount(String handle, String description) {
+        return 4;
+    }
+
+    public void removeAccount(int handle) {
+        //todo
+    }
+
+    public void removeAccount(String handle) {
+        // to do
+    }
+
+    public void changeAccountHandle(String oldHandle, String newHandle) {
+        //to do
+    }
+
+    public void updateAccountDescription(String handle, String description) {
+        //to do
+    }
+
+    public String showAccount(String handle) {
+        // to do
+        return "hello there";
+    }
+
+}
---
+++ src/socialmedia/Post.java
@@ -0,0 +1,27 @@
+package socialmedia;
+import java.util.ArrayList;
+
+
+public class Post {
+    private int id;
+    private String message; //100 character limit
+    private Account author; //Object account? 
+    private ArrayList<Integer> comments = new ArrayList<Integer>();
+
+
+    public int createPost(String handle, String message) {
+        return 4;
+    }
+
+    public int endorsePost(String handle, int id) {
+        return 4;
+    }
+
+    public int commentPost(String handle, int id, String message) {
+        return 4;
+    }
+
+    public void deletePost(int id) {
+        //to do
+    }
+}
